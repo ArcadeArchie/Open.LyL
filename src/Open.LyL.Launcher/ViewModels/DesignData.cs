92ad962ba6cd02bcae4f86862d5985d28231659a
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2023-11-03 19:22:34 +0100

    Add Helper class for the designer

---
+++ src/Open.LyL.Launcher/ViewModels/DesignData.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia;
+using Microsoft.Extensions.DependencyInjection;
+using Open.LyL.Launcher.ViewModels;
+
+namespace Open.LyL.Launcher;
+
+public static class DesignData
+{
+    public static MainWindowViewModel MainWindowViewModel { get; } =
+        ((App)Application.Current!).GlobalHost!.Services.GetRequiredService<MainWindowViewModel>();
+}
