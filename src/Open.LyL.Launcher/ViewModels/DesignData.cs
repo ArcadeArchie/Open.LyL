1a9e98ddc125f3e8d2777469f2674950a592d503
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2023-11-06 20:15:44 +0100

    Add some comments and demo code

--- src/Open.LyL.Launcher/ViewModels/DesignData.cs
+++ src/Open.LyL.Launcher/ViewModels/DesignData.cs
@@ -4,6 +4,9 @@ using Open.LyL.Launcher.ViewModels;
 
 namespace Open.LyL.Launcher;
 
+/// <summary>
+/// Used by for the Designer as a source of generated view models
+/// </summary>
 public static class DesignData
 {
     public static MainWindowViewModel MainWindowViewModel { get; } =
--- src/Open.LyL.Launcher/ViewModels/MainWindowViewModel.cs
+++ src/Open.LyL.Launcher/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.Extensions.Logging;
+using ReactiveUI;
+using System;
+using System.Linq;
 
 namespace Open.LyL.Launcher.ViewModels;
 
-public class MainWindowViewModel : ViewModelBase
+public class MainWindowViewModel : ViewModelBase, IActivatableViewModel
 {
     private readonly ILogger<MainWindowViewModel> _logger;
 
     public string Greeting => "Welcome to Avalonia!";
 
+    public ViewModelActivator Activator { get; } = new();
+
     public MainWindowViewModel(ILogger<MainWindowViewModel> logger)
     {
         _logger = logger;
+        this.WhenActivated(() =>
+        {
+            _logger.LogInformation("MainVM greeted '{greeting}'", Greeting);
+            return Enumerable.Empty<IDisposable>();
+        });
     }
 }
